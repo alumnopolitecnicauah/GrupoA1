3c688f39e284e9667f98bdb5a4819c21732e38ae
Rodrigo <roy_casta@hotmail.com>
2025-09-06 21:16:37 +0200

    Nombre: Garcia Garcia, Eliseo

--- primos.c
+++ primos.c
@@ -2,8 +2,9 @@
 #include <math.h>
 uint8_t esprimo(uint16_t primo){
 uint16_t divisor;
-uint16_t divisores=0;
+
 	
+
 	for(divisor=sqrt(primo+1);divisor>=2;divisor--){
 		if((primo%divisor)==0){
 			divisores++;
@@ -11,6 +12,5 @@ uint16_t divisores=0;
 }
 if(divisores == 0)
 	return 1;
-else
-	return 0;
+
 }
